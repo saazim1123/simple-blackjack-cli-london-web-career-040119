de769119736f3572712a3776596b7e296e52540d
Adnan Azim <saazim1123@gmail.com>
2019-03-09 23:54:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,7 @@ end
 
 def end_game(card_total)
   # code #end_game here
-  puts "Apology your total is #{card_total}. Thanks for playing"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
